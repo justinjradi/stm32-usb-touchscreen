703aae5c84d5abe755ef98f32cc7f90f43b2b302
justinjradi <71425588+justinjradi@users.noreply.github.com>
2024-06-24 16:01:02 -0700

    This is what I'd do for hybrid mode but this isn't hybrid mode so I should simplify

--- USB_DEVICE/App/usb_device.c
+++ USB_DEVICE/App/usb_device.c
@@ -47,12 +47,13 @@ typedef struct __attribute__((packed))
 typedef struct __attribute__((packed))
 {
 	uint8_t report_ID;
-	Contact contacts[TOUCHSCREEN_MAX_CONTACTS];
+	Contact reported_contacts[TOUCHSCREEN_MAX_CONTACTS];
 	uint16_t scan_time;
 	uint8_t contact_count;
 } TouchReport;	// touch report size = 4 + (contact size * TOUCHSCREEN_MAX_CONTACTS)
 
-Contact* contacts[TOUCHSCREEN_MAX_CONTACTS];
+Contact contacts_by_ID[TOUCHSCREEN_MAX_CONTACTS];
+uint8_t is_reported[TOUCHSCREEN_MAX_CONTACTS];
 /* USER CODE END PV */
 
 /* USER CODE BEGIN PFP */
@@ -77,46 +78,43 @@ USBD_HandleTypeDef hUsbDeviceFS;
 
 void touchscreen_set(uint8_t contact_ID, uint16_t x, uint16_t y)
 {
-	if (contacts[contact_ID] == NULL)
-	{
-		contacts[contact_ID] = (Contact*)calloc(sizeof(Contact));
-	}
-	*contacts[contact_ID].tip_switch = 1;
-	*contacts[contact_ID].x = x;
-	*contacts[contact_ID].y = y;
+	contacts_by_ID[contact_ID].tip_switch = 1;
+	contacts_by_ID[contact_ID].x = x;
+	contacts_by_ID[contact_ID].y = y;
 }
 
 void touchscreen_reset(uint8_t contact_ID)
 {
-	*contacts[contact_ID].tip_switch = 0;
+	contacts_by_ID[contact_ID].tip_switch = 0;
 }
 
-int touchscreen_update(uint16_t scan_time)
+void touchscreen_update(int scan_time)
 {
 	TouchReport touchReport;
+	int reporting_order = 0;
+	// Fill list of reported contacts
+	for (int id = 0; id < TOUCHSCREEN_MAX_CONTACTS; id++)
+	{
+		if (is_reported[id])
+		{
+			touchReport.reported_contacts[reporting_order] = contacts_by_ID[id];
+			reporting_order++;
+		}
+		// Remove recently or current reset contacts from being reported next time function is called
+		if (contacts_by_ID[id].tip_switch == 0)
+		{
+			is_reported[id] = 0;
+		}
+	}
+	// Clear remaining contact slots in report
+	for (int j = reporting_order; j < TOUCHSCREEN_MAX_CONTACTS; j++)
+	{
+		memset(touchReport.reported_contacts(j), 0, sizeof (Contact));
+	}
+	// Set remaining parameters and send report
 	touchReport.report_ID = REPORTID_TOUCH;
-	touchReport.scan_time = scan_time;
-	int contact_count = 0;
-	int j = 0;
-	for (int i = 0; i < TOUCHSCREEN_MAX_CONTACTS; i++)
-	{
-		if (contacts[i] == NULL)
-		{
-			continue;
-		}
-		touchReport.contacts[j] = *contacts[i];
-		j++;
-		if (*contacts[i].tip_switch == 0)
-		{
-			free(contacts[i]);
-			contacts[i] = NULL;
-		}
-	}
-	for (j; j < TOUCHSCREEN_MAX_CONTACTS; j++)
-	{
-		memset(touchReport.contacts[j], 0, sizeof (Contact));
-	}
-	touchReport.contact_count = contact_count;
+	touchReport.scan_time = (uint16_t) (scan_time * 10);	// Convert 100μs units to 1ms units and let overflow wrap around
+	touchReport.contact_count = contact_reporting_order;
 	USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, (uint8_t *) &touchReport, sizeof (touchReport));
 }
 
